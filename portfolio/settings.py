551d5879a17854f56ac38b6c0c414ee6c9d580e2
Carlos Ramirez <carlosarg54@gmail.com>
2017-04-10 18:27:11 -0400

    Added api app

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #My apps
+    'apps.projects',
+    'apps.api',
+
+    #Third party
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
